70ee59b740827f62df33e57f239c742ef8a23851
Metin Alperen UÇAN <metinalperenucan@gmail.com>
2025-08-06 12:57:38 +0300

    backend ile bağlantı yapıldı

--- restoran-yonetim-sistemi/vite.config.js
+++ restoran-yonetim-sistemi/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.232.113:8080', // Spring Boot backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
